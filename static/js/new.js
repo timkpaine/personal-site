382e4a1d7fd963dcdf9f8040ea7aef0b00aaddbc
Tim Paine <t.paine154@gmail.com>
2015-07-11 15:53:13 -0400

    updates to personal website.

---
+++ static/js/new.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+/*
+   $(".maystreet").mouseenter(function() {
+        $(this).fadeTo('fast',1);
+    });
+    $(".maystreet").mouseleave(function() {
+        $(this).fadeTo('fast',0.3);
+    });
+*/
+});
